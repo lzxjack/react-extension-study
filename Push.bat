62b80915b529164acfb2cdc026c004495659792f
Jack <965555169@qq.com>
2021-04-21 10:04:42 +0800

    UPDATA

--- Push.bat
+++ Push.bat
@@ -1,4 +1,4 @@
 @echo off
 git add .
-git commit -m README
+git commit -m UPDATA
 git push
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react';
+import Demo from './components/3_hooks';
+
+export default class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Demo />
+            </Fragment>
+        );
+    }
+}
